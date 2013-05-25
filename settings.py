37d11160f3bcafe0facd3ba63f7eade4f3380853
moodpo <yangxiaoxiehaha@gmail.com>
2013-05-26 00:36:20 +0800

    基本页面提交

---
+++ settings.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+name = 'moodpo'
+version = '0.10'
+website = 'https://github.com/moodpo/liquidluck-theme-moodpo'
+author = {
+    'name': 'yangxiaoxie',
+    'website': 'http://moodpo.com'
+}
+
+lang = 'zh-CN'
+
+siteTitle = 'Moodpo Blog'
+
+siteDescriptions = '记录生活点滴，感悟程序人生'
+
+navigation = [
+    {'title': 'Home', 'link': 'http://moodpo.com'},
+    {'title': 'Work', 'link': '/work.html'},
+    {'title': 'Life', 'link': '/life.html'},
+    {'title': 'About', 'link': '/about.html'},
+]
+
+
+
